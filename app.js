6a124b5a267b8da06f6feec29975927ae19dfa34
Mariela Torres <martz_maru@hotmail.com>
2019-07-24 18:43:41 -0500

    Añadiendo menu

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-js
+// $(document).ready(function() {
+//     $(".submenu").hide();
+//     $(".contenedor--menu").hide();
+    
+//        $(".icono").click(function() {
+//         $(".contenedor--menu").animate({
+//                 width: "toggle"
+                     
+//             });
+//       });   
+      
+//     $( ".submenu" ).before(innerHTML = "\u25bc");
+//         $('.submenu')
+//         //despliega solo el submenu de ese menu concreto
+//         $('.menu__enlace').click(function(event){
+//             var elem = $(this).next();
+        
+//             if(elem.is('ul')){          
+//                 event.preventDefault();
+//                 elem.slideToggle();
+//             }
+//         });
+    
+//     });//fin de la funcion ready
+
+// $("#menu-toggle").click(function(e) {
+//    e.preventDefault();
+//     $("#wrapper").toggleClass("toggled");
+//   });
